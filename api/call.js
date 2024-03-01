6995eaa6f0ae65dd79f041a67d53d02efd635346
Jambo <jambo159.9coder@gmail.com>
2024-03-01 14:15:49 +0800

    update api name

--- api/call.js
+++ api/call.js
@@ -2,10 +2,11 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-    const { pageLimit, network, collection, chainId, page } = req.query; // 注意：使用req.query接收GET请求的参数
+    // 从请求体中解构参数
+    const { pageLimit, network, collection, chainId, page } = req.body;
 
     const url = `https://v3-api.lootex.io/api/v3/explore/assets?limit=${pageLimit}&collectionSlugs%5B0%5D=${network}%3A${collection}&sortBy=bestListPrice&chainId=${chainId}&page=${page}`;
-
+    
     try {
         const response = await axios.get(url);
         res.json(response.data);
